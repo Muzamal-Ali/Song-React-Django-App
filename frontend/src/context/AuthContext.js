877211537fc0da191f3e2391910c79e7a29af326
Muzamal-Ali <muzamal.ali@mlsense.ai>
2023-05-04 12:34:47 +0500

    initial commit

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -17,7 +17,7 @@ export const AuthProvider = ({children}) => {
 
     let loginUser = async (e )=> {
         e.preventDefault()
-        let response = await fetch('http://127.0.0.1:8000/api/token/', {
+        let response = await fetch('https://muzamal-django-dot-cloud-work-314310.ew.r.appspot.com/api/token/', {
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
