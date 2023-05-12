608b1f435665e6220dc7950fc333d917366b4a4b
Muzamal-Ali <muzamal.ali@mlsense.ai>
2023-05-12 17:26:13 +0500

    initial commit

--- frontend/src/context/config.js
+++ frontend/src/context/config.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'https://muzamal-django-dot-cloud-work-314310.ew.r.appspot.com'; 
-const API_BASE_URL = 'http://127.0.0.1:8000/'; // Replace with your API's base URL
+const API_BASE_URL = 'https://muzamal-django-dot-cloud-work-314310.ew.r.appspot.com'; 
+// const API_BASE_URL = 'http://127.0.0.1:8000/'; // Replace with your API's base URL
 
 
 export default API_BASE_URL;
